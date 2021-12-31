2b35d47f0f7ecd1b8751aba65ff03e4a5096145d
PDPN <69457861+PDPN@users.noreply.github.com>
2021-12-31 17:04:32 +0530

    Add files via upload

---
+++ score.py
@@ -0,0 +1,30 @@
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("Admission_Predict.csv")
+
+toefl_score = df["TOEFL Score"].tolist()
+result = df["Chance of admit"].tolist()
+
+fig = px.scatter(x=toefl_score, y=result)
+fig.show()
+
+toefl_score = df["TOEFL Score"].tolist()
+gre_score = df["GRE Score"].tolist()
+
+results = df["Chance of admit"].tolist()
+colors=[]
+for data in results:
+  if data == 1:
+    colors.append("green")
+  else:
+    colors.append("red")
+
+fig = go.Figure(data=go.Scatter(
+    x=toefl_score,
+    y=gre_score,
+    mode='markers',
+    marker=dict(color=colors)
+))
+fig.show()
